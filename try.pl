eb45a5b0cbc720f09db4301f490b1814bfec75df
yuweny1 <92225023+yuweny1@users.noreply.github.com>
2023-02-26 17:11:28 +0000

    Update cardero.txt

--- try.pl
+++ try.pl
@@ -6,4 +6,12 @@ use warnings;
 use lib qw(/home/cindy/perl5/lib/perl);
 use Data::Dumper;
 
-us
+use Router::R3;
+
+#Router::R3::test();
+my $t = Router::R3->new(
+    '/foo/bar' => 2,
+    '/zoo' => 1,
+    '/bar' => 3,
+    '/post/{id}' => 4,
+  
